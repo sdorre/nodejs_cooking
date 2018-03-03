477a4e392d499c729cf206f02a798bbce14d8b40
sdorre <stephane.dorre@gmail.com>
2018-03-03 11:30:15 +0100

    Add Edition and Deletion feature

--- src/app/recipe-create/recipe-create.component.ts
+++ src/app/recipe-create/recipe-create.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-recipe-create',
-  templateUrl: './recipe-create.component.html',
-  styleUrls: ['./recipe-create.component.css']
+    selector: 'app-recipe-create',
+    templateUrl: './recipe-create.component.html',
+    styleUrls: ['./recipe-create.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
+
 export class RecipeCreateComponent implements OnInit {
 
-  constructor() { }
+    recipe = {};
+    
+    constructor(private http: HttpClient, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    saveRecipe() {
+        this.http.post('/recipe', this.recipe)
+            .subscribe(res => {
+                    let id = res['_id'];
+                    this.router.navigate(['/recipe-details', id]);
+                }, (err) => {
+                    console.log(err);
+                }
+            );
+    }
 }
--- src/app/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+    selector: 'app-recipe-edit',
+    templateUrl: './recipe-edit.component.html',
+    styleUrls: ['./recipe-edit.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+    recipe: any = {};
 
-  ngOnInit() {
-  }
+    constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
 
+    ngOnInit() {
+        this.getRecipe(this.route.snapshot.params['id']);
+    }
+
+    getRecipe(id) {
+        this.http.get('/recipe/'+id)
+            .subscribe(data => {
+                this.recipe = data;
+            });
+    }
+
+    updateRecipe(id) {
+        this.recipe.updated_date = Date.now();
+        this.http.put('/recipe/'+id, this.recipe)
+            .subscribe(res => {
+                let id = res['_id'];
+                this.router.navigate(['recipe-details', id]);
+            }, (err) => {
+                console.log(err);
+            });
+    }
 }
